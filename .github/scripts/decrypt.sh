aac20867a5f91eadc13dc5fd165b0ccdd9d322f8
Sai Ranjit Tummalapalli <34263716+sairanjit@users.noreply.github.com>
2021-10-23 17:45:59 +0530

    Update decrypt.sh

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -4,8 +4,8 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 
-gpg --quiet --batch --yes --decrypt --passphrase="$KEYS_KEYSTORE_PASSPHRASE" \
+gpg --pinentry-mode loopback --quiet --batch --yes --decrypt --passphrase="$KEYS_KEYSTORE_PASSPHRASE" \
 --output android/app/release-key.keystore android/app/release-key.keystore.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$KEYS_KEYSTORE_PASSPHRASE" \
+gpg --pinentry-mode loopback --quiet --batch --yes --decrypt --passphrase="$KEYS_KEYSTORE_PASSPHRASE" \
 --output fastlane/keys.properties fastlane/keys.properties.gpg
