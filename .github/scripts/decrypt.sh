7e1a366e8af29efd67dbff13ef12a89f4157dc1b
Sai Ranjit Tummalapalli <34263716+sairanjit@users.noreply.github.com>
2021-10-23 17:32:21 +0530

    Testing CI CD for firebase distribution (#1)
    * chore: added scripts to increment versions
    
    * chore: added changes required for firebase distribution
    
    Co-authored-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

---
+++ .github/scripts/decrypt.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Decrypt the file
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+
+gpg --quiet --batch --yes --decrypt --passphrase="$KEYS_KEYSTORE_PASSPHRASE" \
+--output android/app/release-key.keystore android/app/release-key.keystore.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$KEYS_KEYSTORE_PASSPHRASE" \
+--output fastlane/keys.properties fastlane/keys.properties.gpg
